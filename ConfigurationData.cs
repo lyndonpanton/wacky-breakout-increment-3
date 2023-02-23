8c9c02cf872fbb790d713e1bef2bf1b379c69683
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-23 00:09:09 +0000

    Imported supplied starter files

---
+++ ConfigurationData.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A container for the configuration data
+/// </summary>
+public class ConfigurationData
+{
+    #region Fields
+
+    const string ConfigurationDataFileName = "ConfigurationData.csv";
+
+    // configuration data
+    float paddleMoveUnitsPerSecond = 10;
+    float ballImpulseForce = 200;
+    float ballLifeSeconds = 10;
+    float minSpawnSeconds = 5;
+    float maxSpawnSeconds = 10;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the paddle move units per second
+    /// </summary>
+    /// <value>paddle move units per second</value>
+    public float PaddleMoveUnitsPerSecond
+    {
+        get { return paddleMoveUnitsPerSecond; }
+    }
+
+    /// <summary>
+    /// Gets the impulse force to apply to move the ball
+    /// </summary>
+    /// <value>impulse force</value>
+    public float BallImpulseForce
+    {
+        get { return ballImpulseForce; }    
+    }
+
+    /// <summary>
+    /// Gets the number of seconds the ball lives
+    /// </summary>
+    /// <value>ball life seconds</value>
+    public float BallLifeSeconds
+    {
+        get { return ballLifeSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the minimum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>minimum spawn seconds</value>
+    public float MinSpawnSeconds
+    {
+        get { return minSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the maximum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>maximum spawn seconds</value>
+    public float MaxSpawnSeconds
+    {
+        get { return maxSpawnSeconds; }
+    }
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Constructor
+    /// Reads configuration data from a file. If the file
+    /// read fails, the object contains default values for
+    /// the configuration data
+    /// </summary>
+    public ConfigurationData()
+    {
+        
+    }
+
+    #endregion
+}
